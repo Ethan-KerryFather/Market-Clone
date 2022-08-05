a2514197937a11345d3ec83cef38e027e62d3348
SwoftCreation <limsoft0401@gmail.com>
2022-08-06 02:25:49 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import Header from "./components/Header";
 import Orders from "./components/Orders";
 import Footer from "./components/Footer";
 import Prototypes from "./components/Prototypes";
+import AppStateProvider from "./providers/AppStateProvider";
 
 function App() {
   return (
-    <React.Fragment>
+    <AppStateProvider>
       <Header />
       <div className="container">
         <Prototypes />
         <Orders />
         <Footer />
       </div>
-    </React.Fragment>
+    </AppStateProvider>
   );
 }
 
--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,10 +1,55 @@
+import useActions from "../hooks/useActions";
+import useOrders from "../hooks/useOrders";
+import usePrototypes from "../hooks/usePrototypes";
+
 export default function Orders() {
-  return (
-    <aside>
-      <div className="empty">
-        <div className="title">You don't have any orders </div>
-        <div className="subtitle">Click this to add an order</div>
-      </div>
-    </aside>
-  );
+  const orders = useOrders();
+  console.log(orders);
+  const prototypes = usePrototypes();
+  const { remove } = useActions();
+  if (orders.length === 0) {
+    return (
+      <aside>
+        <div className="empty">
+          <div className="title">You don't have any orders </div>
+          <div className="subtitle">Click this to add an order</div>
+        </div>
+      </aside>
+    );
+  } else {
+    return (
+      <aside>
+        <div className="order">
+          <div className="body">
+            {orders.map((element) => {
+              const { id } = element;
+              // 구조분해할당
+              const prototype = prototypes.find((element) => element.id === id);
+              const click = () => {
+                remove(id);
+              };
+              return (
+                <div className="item" key={id}>
+                  <div className="img">
+                    <video src={prototype.thumbnail} />
+                  </div>
+                  <div className="content">
+                    <p className="title">
+                      {prototype.title} x {element.quantity}
+                    </p>
+                    <p className="price">
+                      ${prototype.price * element.quantity}
+                    </p>
+                    <button className="btn btn--link" onClick={remove(id)}>
+                      <i className="icon icon--cross" />
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </aside>
+    );
+  }
 }
